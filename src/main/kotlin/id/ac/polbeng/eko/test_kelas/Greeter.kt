f32edeec3a321bfb3e2924ca90f8592573f4980d
Eko Candra <yteko12@gmail.com>
2022-03-24 10:38:19 +0700

    contoh penggunaan kelas di kotlin

---
+++ src/main/kotlin/id/ac/polbeng/eko/test_kelas/Greeter.kt
@@ -0,0 +1,14 @@
+package id.ac.polbeng.eko.test_kelas
+
+class Greeter {
+    fun greet() {
+        println("Hello object world!")
+    }
+
+    /**
+     * Header ditandai dengan tanda kurung berwarna merah dan biasanya didalam header tersebut
+    terdapat parameter yang selanjutnya disebut juga dengan primary constructor. Pasangan kurung
+    kurawal berwarna biru adalah bagian tubuh (body) suatu kelas. Header dan body bersifat
+    opsional artinya keberadaanya boleh ada atau tidak.
+     */
+}
