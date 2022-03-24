dab6c72ed79b4da259dd94982771ef45ba0bb3a0
Eko Candra <yteko12@gmail.com>
2022-03-24 10:54:03 +0700

    contoh penerapan initializer blok

---
+++ src/main/kotlin/id/ac/polbeng/eko/test_kelas/Initializer_Block.kt
@@ -0,0 +1,21 @@
+package id.ac.polbeng.eko.test_kelas
+
+class InitOrder(name: String) {
+    val firstProperty = "First property: $name".also(::println)
+    init {
+        println("First initializer block that prints ${name}")
+    }
+    val secondProperty = "Second property: ${name.length}".also(::println)
+    init {
+        println("Second initializer block that prints ${name.length}")
+    }
+
+    /**
+     * Blok inisialisasi diawali dengan keyword init. Selama instansiasi sebuah objek, urutan eksekusi
+    blok inisialisasi sama dengan urutan yang ada pada tubuh kelas, berselangkan inisialisasi
+    property-nya.
+     */
+}
+fun main(){
+    val initOrder = InitOrder("RPL")
+}
