206e5adf5b301a43f0c143f5e05f1232d25f4a07
Eko Candra <yteko12@gmail.com>
2022-03-24 11:03:59 +0700

    contoh penerapan Secondary construktor

---
+++ src/main/kotlin/id/ac/polbeng/eko/test_kelas/Secondary_Constructor.kt
@@ -0,0 +1,26 @@
+package id.ac.polbeng.eko.test_kelas
+
+class PersonD {
+    var firstName: String
+    var lastName: String
+    var age: Int
+    constructor(_firstName: String, _lastName: String, _age: Int){
+        firstName = _firstName
+        lastName = _lastName
+        age = _age
+    }
+
+}
+fun main(){
+    val budi = PersonD("Budi", "Gunawan", 21)
+    println("Name : ${budi.firstName} ${budi.lastName}")
+    println("Age : ${budi.age}")
+
+    /**
+     * Konstruktor tambahan (secondary constructor) didefenisikan didalam tubuh kelas. Jika property
+    kelas tidak diinisialisasi pada saat mendeklarasikannya maka property tersebut harus di
+    inisialisasi didalam konstruktor tambahan atau blok inisialisasi. Konstruktor tambahan
+    memerlukan keyword constructor untuk mendefenisikannya. Selain itu konstruktor tambahan
+    boleh tidak memiliki tubuh kelas dalam pendefenisiannya.
+     */
+}
