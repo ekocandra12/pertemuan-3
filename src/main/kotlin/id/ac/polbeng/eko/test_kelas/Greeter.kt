5d88856ff6648396712cf45202decf52bc4af590
Eko Candra <yteko12@gmail.com>
2022-03-24 15:09:49 +0700

    contoh penerapan kelas dan objek

--- src/main/kotlin/id/ac/polbeng/eko/test_kelas/Greeter.kt
+++ src/main/kotlin/id/ac/polbeng/eko/test_kelas/Greeter.kt
@@ -9,6 +9,6 @@ class Greeter {
      * Header ditandai dengan tanda kurung berwarna merah dan biasanya didalam header tersebut
     terdapat parameter yang selanjutnya disebut juga dengan primary constructor. Pasangan kurung
     kurawal berwarna biru adalah bagian tubuh (body) suatu kelas. Header dan body bersifat
-    opsional artinya keberadaanya boleh ada atau tidak
+    opsional artinya keberadaanya boleh ada atau tidak.
      */
 }
