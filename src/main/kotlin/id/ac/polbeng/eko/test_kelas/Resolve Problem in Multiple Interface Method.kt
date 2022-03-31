bfbcc9999505c41ebc285ccbaec0f1dea7412d73
Eko Candra <yteko12@gmail.com>
2022-03-31 10:10:01 +0700

    contoh penerapan resolve problem in multiple interface method

---
+++ src/main/kotlin/id/ac/polbeng/eko/test_kelas/Resolve Problem in Multiple Interface Method.kt
@@ -0,0 +1,28 @@
+package id.ac.polbeng.eko.test_kelas
+
+interface A {
+    fun demo() {
+        println("From interface A")
+    }
+}
+interface B {
+    fun demo() {
+        println("From interface B")
+    }
+}
+// This class implements X and Y interfaces
+class MyClassB: A, B {
+    override fun demo() {
+        super<A>.demo()
+        super<B>.demo()
+    }
+}
+
+fun main() {
+    val obj = MyClassB()
+    obj.demo()
+
+    /**
+     * berikut adalah penerapa resolve problem in multiple interface method
+     */
+}
