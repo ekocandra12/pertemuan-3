3f7a6627349c249910a60f5376023605573f118e
Eko Candra <yteko12@gmail.com>
2022-03-31 10:23:35 +0700

    contoh penerapan Data Classes and Destructuring Declarations:

---
+++ src/main/kotlin/id/ac/polbeng/eko/test_kelas/Data Classes and Destructuring Declarations.kt
@@ -0,0 +1,14 @@
+package id.ac.polbeng.eko.test_kelas
+
+fun main() {
+    val meri = Student("Meri", 20)
+// Destructuring Declaration
+    val(name, age) = meri
+    println("Name = $name")
+    println("Age = $age")
+
+    /**
+     * Kita dapat mendestruksi data kelas sebuah objek kedalam sejumlah variabel menggunakan
+    destructing declarations.
+     */
+}
